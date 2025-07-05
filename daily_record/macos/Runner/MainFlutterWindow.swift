dbc4930c3f32b8d3227eab1b458e5610f7e2edf6
shimizu.kenji <shimizu.kenji@noncore.net>
2025-07-05 17:07:45 +0900

    ウィンドウの最小サイズを設定する機能を追加

--- daily_record/macos/Runner/MainFlutterWindow.swift
+++ daily_record/macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,9 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    // ウィンドウの最小サイズを設定
+    self.minSize = NSSize(width: 400, height: 600)
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
